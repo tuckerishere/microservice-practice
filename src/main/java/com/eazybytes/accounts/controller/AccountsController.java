966bd78196158d946e2da6acf9ab8bd84f4ea059
tuckerishere <michael.tucker.a@gmail.com>
2024-07-30 20:03:42 -0500

    test commit

--- src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -7,10 +7,7 @@ import com.eazybytes.accounts.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +26,4 @@ public class AccountsController {
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
-
 }
